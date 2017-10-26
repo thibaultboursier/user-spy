0d304c660ddd79d44bc33d7d818071edf30387de
Thibault Boursier <boursier.thibault@gmail.com>
2017-10-27 00:55:49 +0200

    WIP: add server

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Glue = require('glue');
+const manifest = require('./config/manifest.json');
+const options = {
+    relativeTo: `${__dirname}/plugins`
+};
+
+Glue.compose(manifest, options)
+    .then(server => {
+        server.start(err => {
+            if (err) throw err;
+            
+            console.log('Application is running.');
+        })
+    });
